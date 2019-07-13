f3f5003ccf416cf471dfa1749caf6ae4279f870d
Allen Liu <kamyu104@gmail.com>
2019-07-14 04:48:08 +0800

    update

---
+++ Round 2/on_the_run.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2019 kamyu. All rights reserved.
+#
+# Facebook Hacker Cup 2019 Round 2 - On the Run
+# https://www.facebook.com/hackercup/problem/432000547357525/
+#
+# Time:  O(1)
+# Space: O(1)
+#
+            
+def on_the_run():
+    N, M, K = map(int, raw_input().strip().split())
+    states = set()
+    for _ in xrange(K+1):
+        states.add(sum(map(int, raw_input().strip().split())) % 2)
+
+    return "Y" if K == 2 and len(states) == 1 else "N"
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, on_the_run())
