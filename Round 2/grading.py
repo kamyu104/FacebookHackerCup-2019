a9af208aeddecd74ffaf9e0d0657a638cf8c2b37
Allen Liu <kamyu104@gmail.com>
2019-07-15 02:29:14 +0800

    update

--- Round 2/grading.py
+++ Round 2/grading.py
@@ -18,7 +18,7 @@ def grading():
     L = map(int, raw_input().strip().split())
     INF = S*H-1
 
-    # min_discard_with_f[f][s]: min total discards for at most s context switches assuming first paper type is f
+    # min_discard_with_f[f][s]: min total discards for at most s context switches assuming first graded paper type is f
     min_discard_with_f = [[0 for _ in xrange(H+1)] for _ in xrange(2)]
     # dp[i][p][s]: min discards for at most s context switches in the first i papers in stack,
     #              with current paper type being p
