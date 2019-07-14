43fb11f1a2f82cf56db2ebb5fdcae524f6d8d182
Allen Liu <kamyu104@gmail.com>
2019-07-15 00:04:12 +0800

    update

--- Round 2/grading.py
+++ Round 2/grading.py
@@ -15,8 +15,11 @@ def grading():
     L = map(int, raw_input().strip().split())
     INF = S*H-1
 
-    minD = [[0 for _ in xrange(H+1)] for _ in xrange(2)]
-    dp = [[[INF for _ in xrange(2)] for _ in xrange(H+2)] for _ in xrange(2)]
+    # min_discard[f][s]: min total discards for at most s context switches assuming first paper type is f
+    min_discard = [[0 for _ in xrange(H+1)] for _ in xrange(2)]
+    # dp[i][s][p]: min discards for at most s context switches in the first i papers in stack,
+    #              with current paper type being p
+    dp = [[[INF for _ in xrange(2)] for _ in xrange(H+2)] for _ in xrange(2)]  
     for f in xrange(2):
         for j in xrange(S):
             for s in xrange(H+2):
@@ -34,15 +37,16 @@ def grading():
                         dp[(i+1)%2][s+int(p2 != p)][p2] = min(dp[(i+1)%2][s+int(p2 != p)][p2], d)
                         dp[(i+1)%2][s][p] = min(dp[(i+1)%2][s][p], d+1)
             for s in xrange(H+1):
-                minD[f][s] += min(dp[H%2][s][0], dp[H%2][s][1])
+                min_discard[f][s] += min(dp[H%2][s][0], dp[H%2][s][1])
     
-    result = [INF for _ in xrange(S*H+1)]
+    # min_switch[d]: min total context switches for at most d discards
+    min_switch = [INF for _ in xrange(S*H+1)]
     for f in xrange(2):
         for s in xrange(H+1):
-            result[minD[f][s]] = min(result[minD[f][s]], s)
-    for c in xrange(S*H):
-        result[c+1] = min(result[c+1], result[c])
-    return " ".join(map(lambda c: str(result[c]+1), L))
+            min_switch[min_discard[f][s]] = min(min_switch[min_discard[f][s]], s)
+    for d in xrange(S*H):
+        min_switch[d+1] = min(min_switch[d+1], min_switch[d])
+    return " ".join(map(lambda d: str(min_switch[d]+1), L))
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, grading())
