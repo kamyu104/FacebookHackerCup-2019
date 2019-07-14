230a33384f1c2947db984eb7a5c290f61d45ce0a
Allen Liu <kamyu104@gmail.com>
2019-07-15 01:54:45 +0800

    update

--- Round 2/grading.py
+++ Round 2/grading.py
@@ -15,8 +15,8 @@ def grading():
     L = map(int, raw_input().strip().split())
     INF = S*H-1
 
-    # min_discard[f][s]: min total discards for at most s context switches assuming first paper type is f
-    min_discard = [[0 for _ in xrange(H+1)] for _ in xrange(2)]
+    # min_discard_with_f[f][s]: min total discards for at most s context switches assuming first paper type is f
+    min_discard_with_f = [[0 for _ in xrange(H+1)] for _ in xrange(2)]
     # dp[i][p][s]: min discards for at most s context switches in the first i papers in stack,
     #              with current paper type being p
     dp = [[[INF for _ in xrange(H+2)] for _ in xrange(2)] for _ in xrange(2)]  
@@ -38,16 +38,19 @@ def grading():
                         dp[(i+1)%2][p2][s+int(p2 != p)] = min(dp[(i+1)%2][p2][s+int(p2 != p)], d)
                         dp[(i+1)%2][p][s] = min(dp[(i+1)%2][p][s], d+1)
             for s in xrange(H+1):
-                min_discard[f][s] += min(dp[H%2][0][s], dp[H%2][1][s])
-    
+                min_discard_with_f[f][s] += min(dp[H%2][0][s], dp[H%2][1][s])
+
+    # min_discard[s]: min total discards for at most s context switches
+    min_discard = [INF for _ in xrange(H+1)]
+    for s in xrange(H+1):
+        min_discard[s] = min(min_discard_with_f[0][s], min_discard_with_f[1][s])
     # min_switch[d]: min total context switches for at most d discards
     min_switch = [INF for _ in xrange(S*H+1)]
-    for f in xrange(2):
-        for s in xrange(H+1):
-            min_switch[min_discard[f][s]] = min(min_switch[min_discard[f][s]], s)
-    for d in xrange(S*H):
+    for s in xrange(H+1):
+        min_switch[min_discard[s]] = min(min_switch[min_discard[s]], s)
+    for d in xrange(S*H):  # make every discard with correct min switches
         min_switch[d+1] = min(min_switch[d+1], min_switch[d])
-    return " ".join(map(lambda d: str(min_switch[d]+1), L))
+    return " ".join(map(lambda d: str(min_switch[d]+1), L))  # 1-based count
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, grading())
