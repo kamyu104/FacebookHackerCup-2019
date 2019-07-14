981a21480774b419c769ea34c1fd7e297bdf228e
Allen Liu <kamyu104@gmail.com>
2019-07-14 18:42:03 +0800

    update

--- Round 2/bitstrings_as_a_service.py
+++ Round 2/bitstrings_as_a_service.py
@@ -37,13 +37,15 @@ def bitstrings_as_a_service():
             j -= 1
 
     comp = Counter(map(union_find.find_set, range(N)))
-    dp = [[-1 for _ in xrange(N+1)] for _ in xrange(len(comp)+1)]
+    dp = [[-1 for _ in xrange(N+1)] for _ in xrange(len(comp)+1)]  # dp[i][j] means the back link of 
+                                                                   # the first i component with j nodes labeled 1,
+                                                                   # -1 means impossible
     dp[0][0] = 0
     for i, count in enumerate(comp.itervalues()):
         for j in xrange(N):
             if dp[i][j] != -1:
-                dp[i+1][j] = j
-                dp[i+1][j+count] = j
+                dp[i+1][j] = j  # the first i+1 component is possible with j nodes labeled 1
+                dp[i+1][j+count] = j  # the first i+1 component is also possible with (j + (i+1)_th_comp_size) nodes labeled 1
     for i in xrange(N+1):  # find the min diff
         j = N//2+i
         if dp[-1][j] != -1:
