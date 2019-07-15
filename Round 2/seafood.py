441a2b9d161cf8e09d10451302cc9c9a4bf06094
Allen Liu <kamyu104@gmail.com>
2019-07-15 19:50:38 +0800

    update

--- Round 2/seafood.py
+++ Round 2/seafood.py
@@ -7,65 +7,68 @@
 # Space: O(N)
 #
 
-from bisect import bisect_left
-           
+from bisect import bisect_right
+
 def seafood():
     N = input()
     G = [[None for _ in xrange(N)] for _ in xrange(2)]
     for i in xrange(2):
-        G[i][0], G[i][1], A, B, C, D = map(int, raw_input().strip().split())
+        G[i][0], G[i][1], A, rightmost_harder_R, C, D = map(int, raw_input().strip().split())
         for j in xrange(2, N):
-            G[i][j] = (A*G[i][j-2] + B*G[i][j-1] + C) % D + 1
+            G[i][j] = (A*G[i][j-2] + rightmost_harder_R*G[i][j-1] + C) % D + 1
     O = raw_input().strip()
 
-    P = sorted((G[0][i], (O[i], G[1][i])) for i in xrange(N))
+    P = sorted((G[0][i], O[i], G[1][i]) for i in xrange(N))
     C = []
     for i in xrange(N):
-        if P[i][1][0] == 'C':
+        if P[i][1] == 'C':
             C.append(i)
-    st = []
+    ascending_R_H_from_right = []
     for i in reversed(xrange(C[-1])):
-        if (P[i][1][0] == 'R') and (not st or (P[i][1][1] > st[-1][0])):
-            st.append((P[i][1][1], P[i][0]))
-    B = [-1]*len(C)
+        if (P[i][1] == 'R') and \
+           (not ascending_R_H_from_right or (ascending_R_H_from_right[-1][0] < P[i][2])):
+            ascending_R_H_from_right.append((P[i][2], P[i][0]))
+    rightmost_harder_R = [-1]*len(C)  # rightmost_harder_R[i] = j: rightmost rock j s.t. ascending_R_H_from_right[j] > P[C[i]][2]
     for i in xrange(len(C)):
-        j = bisect_left(st, (P[C[i]][1][1]+1, 0))
-        if j == len(st):
+        j = bisect_right(ascending_R_H_from_right, (P[C[i]][2], float("inf")))
+        if j == len(ascending_R_H_from_right):
             break
-        B[i] = st[j][1]
-    sufCB = [float("inf")]*(len(C)+1)
-    sufCH = [float("-inf")]*(len(C)+1)
+        rightmost_harder_R[i] = ascending_R_H_from_right[j][1]
+
+    suffix_min_B = [float("inf")]*(len(C)+1)
+    suffix_max_H = [float("-inf")]*(len(C)+1)
     for i in reversed(xrange(len(C))):
-        sufCB[i] = min(sufCB[i+1], B[i])
-        sufCH[i] = max(sufCH[i+1], P[C[i]][1][1])
-    fr = []
+        suffix_min_B[i] = min(suffix_min_B[i+1], rightmost_harder_R[i])
+        suffix_max_H[i] = max(suffix_max_H[i+1], P[C[i]][2])
+    ascending_R_H_after_rightmost_C = []
     for i in xrange(C[-1]+1, N):
-        if not fr or P[i][1][1] > fr[-1][0]:
-            fr.append((P[i][1][1], P[i][0]))
+        if not ascending_R_H_after_rightmost_C or \
+           ascending_R_H_after_rightmost_C[-1][0] < P[i][2]:
+            ascending_R_H_after_rightmost_C.append((P[i][2], P[i][0]))
 
     result = float("inf")
-    st = []
+    stk = []
     for i in xrange(len(C)):
         if not i:
             d = 0
         else:
             p = P[C[i-1]][0]
-            while len(st) > 1:
-                a = st[-1][1]
-                b = min(a, st[-2][1])
-                if st[-1][0] + 2*max(0, p-a) < st[-2][0] + 2*max(0, p-b):
+            while len(stk) > 1:
+                a = stk[-1][1]
+                b = min(a, stk[-2][1])
+                if stk[-1][0] + 2*max(0, p-a) < stk[-2][0] + 2*max(0, p-b):
                     break
-                st[-2][1] = b
-                st.pop()
-            d = st[-1][0] + 2*max(0, p-st[-1][1])
-        st.append([d, B[i]])
+                stk[-2][1] = b
+                stk.pop()
+            d = stk[-1][0] + 2*max(0, p-stk[-1][1])
+        stk.append([d, rightmost_harder_R[i]])
         p = P[C[-1]][0]
-        if sufCB[i] >= 0:
-           result =  min(result, d+max(0, p-sufCB[i]))
-        a = bisect_left(fr, (sufCH[i]+1, 0))
-        if a < len(fr):
-            result = min(result, d+(fr[a][1]-p))
-        if B[i] < 0:
+        if suffix_min_B[i] >= 0:
+           result = min(result, d+max(0, p-suffix_min_B[i]))
+        j = bisect_right(ascending_R_H_after_rightmost_C, (suffix_max_H[i], float("inf")))
+        if j != len(ascending_R_H_after_rightmost_C):
+            result = min(result, d+(ascending_R_H_after_rightmost_C[j][1]-p))
+        if rightmost_harder_R[i] == -1:
             break
     return -1 if result == float("inf") else result + P[C[-1]][0]
 
