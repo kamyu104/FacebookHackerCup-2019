5e72d0ccac9ffee17d38fe64da52ef844e1bc860
Allen Liu <kamyu104@gmail.com>
2019-07-15 21:10:21 +0800

    update

--- Round 2/seafood.py
+++ Round 2/seafood.py
@@ -55,7 +55,7 @@ def seafood():
             d = 0
         else:
             prev_p = P[C[i-1]][0]
-            while len(descending_stk) > 1:  # keep potential idx
+            while len(descending_stk) > 1:  # keep potential idxs
                 a = descending_stk[-1][1]
                 b = min(a, descending_stk[-2][1])
                 if descending_stk[-2][0] + 2*max(0, prev_p-b) > \
