14cc72926862912c2bf9f4f8c0d20355a3e41a61
Allen Liu <kamyu104@gmail.com>
2019-07-15 20:19:52 +0800

    update

--- Round 2/seafood.py
+++ Round 2/seafood.py
@@ -23,18 +23,19 @@ def seafood():
     for i in xrange(N):
         if P[i][1] == 'C':
             C.append(i)
-    ascending_R_H_from_right = []
+    ascending_R_H_before_rightmost_C_from_right = []
     for i in reversed(xrange(C[-1])):
         if (P[i][1] == 'R') and \
-           (not ascending_R_H_from_right or (ascending_R_H_from_right[-1][0] < P[i][2])):
-            ascending_R_H_from_right.append((P[i][2], P[i][0]))
+           (not ascending_R_H_before_rightmost_C_from_right or (ascending_R_H_before_rightmost_C_from_right[-1][0] < P[i][2])):
+            ascending_R_H_before_rightmost_C_from_right.append((P[i][2], P[i][0]))
     rightmost_harder_R = [-1]*len(C)  # rightmost_harder_R[i] = j:
-                                      # rightmost rock j s.t. ascending_R_H_from_right[j] > P[C[i]][2]
+                                      # rightmost rock j s.t.
+                                      # ascending_R_H_before_rightmost_C_from_right[j] > P[C[i]][2]
     for i in xrange(len(C)):
-        j = bisect_right(ascending_R_H_from_right, (P[C[i]][2], float("inf")))
-        if j == len(ascending_R_H_from_right):
+        j = bisect_right(ascending_R_H_before_rightmost_C_from_right, (P[C[i]][2], float("inf")))
+        if j == len(ascending_R_H_before_rightmost_C_from_right):
             break
-        rightmost_harder_R[i] = ascending_R_H_from_right[j][1]
+        rightmost_harder_R[i] = ascending_R_H_before_rightmost_C_from_right[j][1]
 
     suffix_min_R = [float("inf")]*(len(C)+1)
     suffix_max_H = [float("-inf")]*(len(C)+1)
@@ -53,23 +54,23 @@ def seafood():
         if not i:
             d = 0
         else:
-            p = P[C[i-1]][0]
+            prev_p = P[C[i-1]][0]
             while len(stk) > 1:
                 a = stk[-1][1]
                 b = min(a, stk[-2][1])
-                if stk[-2][0] + 2*max(0, p-b) > stk[-1][0] + 2*max(0, p-a):
+                if stk[-2][0] + 2*max(0, prev_p-b) > stk[-1][0] + 2*max(0, prev_p-a):
                     break
                 stk[-2][1] = b
                 stk.pop()
-            d = stk[-1][0] + 2*max(0, p-stk[-1][1])
+            d = stk[-1][0] + 2*max(0, prev_p-stk[-1][1])
         stk.append([d, rightmost_harder_R[i]])
-        p = P[C[-1]][0]
+        rightmost_p = P[C[-1]][0]
         if suffix_min_R[i] >= 0:
-           result = min(result, d+max(0, p-suffix_min_R[i]))
+           result = min(result, d+max(0, rightmost_p-suffix_min_R[i]))
         j = bisect_right(ascending_R_H_after_rightmost_C, (suffix_max_H[i], float("inf")))
         if j != len(ascending_R_H_after_rightmost_C):
-            result = min(result, d+(ascending_R_H_after_rightmost_C[j][1]-p))
-        if rightmost_harder_R[i] == -1:
+            result = min(result, d+(ascending_R_H_after_rightmost_C[j][1]-rightmost_p))
+        if rightmost_harder_R[i] == -1:  # no harder rocker or the last clam
             break
     return -1 if result == float("inf") else result + P[C[-1]][0]
 
