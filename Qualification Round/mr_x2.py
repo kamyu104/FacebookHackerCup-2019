1c57e3fc15b8c4dd3aee255ac2bb13ceb726019f
Allen Liu <kamyu104@gmail.com>
2021-05-24 08:00:11 +0800

    update

--- Qualification Round/mr_x2.py
+++ Qualification Round/mr_x2.py
@@ -14,34 +14,22 @@ def calc(operator, x, y):
         return x | y
     return x ^ y
 
-def evaluate(E, curr, lookup):
-    left, operator, right = None, None, None
-    while curr[0] != len(E) and E[curr[0]] != ")":
-        if E[curr[0]] in lookup:
-            if left is None:
-                left = lookup[E[curr[0]]]
-            else:
-                right = lookup[E[curr[0]]]
-            curr[0] += 1
-        elif E[curr[0]] == '(':
-            curr[0] += 1
-            tmp = evaluate(E, curr, lookup)
-            if left is None:
-                left = tmp
-            else:
-                right = tmp
-        else:
-            operator = E[curr[0]]
-            curr[0] += 1
- 
-    if curr[0] != len(E) and E[curr[0]] == ")":
-        curr[0] += 1
-    return calc(operator, left, right) if operator else left
+def evaluate(s, lookup):
+    operands, operators = [], []
+    for i in xrange(len(s)):
+        if s[i] in lookup:
+            operands.append(lookup[s[i]])
+        elif s[i] == ')':
+            right, left = operands.pop(), operands.pop()
+            operands.append(calc(operators.pop(), left, right))
+        elif s[i] != '(':
+            operators.append(s[i])
+    return operands[-1]
 
 def mr_x():
     E = raw_input()
-    return evaluate(E, [0], {'0':0, '1':1, 'x':0 ,'X':1}) ^ \
-           evaluate(E, [0], {'0':0, '1':1, 'x':1 ,'X':0})
+    return evaluate(E, {'0':0, '1':1, 'x':0 ,'X':1}) ^ \
+           evaluate(E, {'0':0, '1':1, 'x':1 ,'X':0})
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, mr_x())
